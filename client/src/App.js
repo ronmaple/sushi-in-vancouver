702131d1167a15edc6793c36f831e92e3f8e7c51
Ron Mapue <ronmapue@gmail.com>
2020-06-21 23:38:52 -0700

    working map

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from "mapbox-gl";
+// import styled from "styled-components";
+// import axios 
+import "mapbox-gl/dist/mapbox-gl.css";
+import "./App.scss";
 
+const mapboxKey = process.env.REACT_APP_MAPBOX_API_KEY
+
+mapboxgl.accessToken = mapboxKey
+
+console.log('mapboxKey: ', mapboxKey, ' typeof: ', typeof mapboxKey);
+
+
+console.log('process.env', process.env.REACT_APP_MAPBOX_API_KEY)
+
+// const MapContainer = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   background: red;
+// `
+// const MapBox = styled.div`
+//   width: 100%;
+//   height: 100%;
+// `
 function App() {
+  const mapboxElRef = useRef(null);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapboxElRef.current,
+      style: "mapbox://styles/ronmaple/ckbq1xlyb1cjc1ipdtut4ahwt",
+      center: [-123.07, 49.29], // Vancouver [lat, long]
+      zoom: 12
+    })
+
+    map.addControl(new mapboxgl.NavigationControl())
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mapContainer">
+        <div className="mapBox" ref={mapboxElRef}>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+{/* <MapContainer className="mapContainer">
+<MapBox className="mapBox" ref={mapboxElRef}>
+
+</MapBox>
+</MapContainer> */}
